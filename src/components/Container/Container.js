d548389fc6ca5c036e47141120e50bb55f0e950f
pdf <pedrodiasfonseca@gmail.com>
2020-05-16 21:33:54 -0300

    feat(prop-types): adds prop-types definitions to components

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styles from './container.module.css'
+import { element, oneOfType, arrayOf } from 'prop-types'
 
 const Container = ({ children }) => (<div className={styles.container}>{children}</div>)
+
+Container.propTypes = {
+  children: oneOfType([
+    element,
+    arrayOf(element)
+  ]).isRequired
+}
 
 export default Container
