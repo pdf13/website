d6be7392f4bf80d0d008928466d2d85852ea7dfb
pdf <pedrodiasfonseca@gmail.com>
2020-05-16 21:25:03 -0300

    feat(i18n): add relate meta-tags to improve SEO

---
+++ src/components/ProfileI18n/ProfileI18n.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+const ProfileI18n = () => (
+  <Head>
+    <link rel='alternate' hreflang='en' href='https://pdf.engineer/en' />
+    <link rel='alternate' hreflang='pt' href='https://pdf.engineer/pt' />
+    <link rel='alternate' hreflang='x-default' href='https://pdf.engineer' />
+  </Head>
+)
+
+export default ProfileI18n
